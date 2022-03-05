fe101233b06f27123f8254ce092a87a0251e8b62
Desolaser <178.felipe@gmail.com>
2022-03-05 20:45:07 -0300

    Add done, edit, remove and setTodos actions to todoReducer

--- reducers/todoReducer.ts
+++ reducers/todoReducer.ts
@@ -4,7 +4,8 @@ type Actions =
   | { type: 'add', payload: { title: string, description: string } }
   | { type: 'done', payload: number }
   | { type: 'remove', payload: number }
-  | { type: 'edit', payload: { id: number, text: string } };
+  | { type: 'edit', payload: { id: number, text: string } }
+  | { type: 'setTodos', payload: Todo[] };
 
 const TodoReducer = (state: Todo[], action: Actions) => {
   switch (action.type) {
@@ -14,9 +15,15 @@ const TodoReducer = (state: Todo[], action: Actions) => {
         title: action.payload.title,  
         description: action.payload.description,
         date: Date.now()      
-      }]
+      }];
     case 'done':
-      return []
+      return state.map(todo => todo.id === action.payload ? { ...todo, isDone: !todo.isDone } : todo);
+    case 'edit':
+      return state.map(todo => todo.id === action.payload.id ? { ...todo, text: action.payload.text } : todo);
+    case 'remove':
+      return state.filter(todo => todo.id !== action.payload);
+    case 'setTodos':
+      return action.payload;
   }
 }
 
