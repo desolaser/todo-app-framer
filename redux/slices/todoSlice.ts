02a8cfffde74806e241e8a994f81db5386a7de23
Felipe Olavarria <178.felipe@gmail.com>
2022-03-25 15:34:37 -0300

    Replace reducer with redux slice

---
+++ redux/slices/todoSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
+import { Todo } from "../../model/Todo";
+
+const initialState: Todo[] = []
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<Todo>) => [...state, action.payload],
+    remove: (state, action: PayloadAction<number>) => 
+      state.filter(todo => todo.id !== action.payload),
+    done: (state, action: PayloadAction<number>) => 
+      state.map(todo => todo.id === action.payload ? { ...todo, isDone: !todo.isDone } : todo),
+    edit: (state, action: PayloadAction<EditTodoPayload>) => 
+      state.map(todo => todo.id === action.payload.id ? action.payload.todo : todo),
+    setTodos: (state, action: PayloadAction<Todo[]>) => 
+      [...action.payload],
+  },
+});
+
+export const { add, remove, done, edit, setTodos } = todoSlice.actions;
+export const selectTodos = (state: RootState) => state.todo;
+export type EditTodoPayload = {
+  id: number;
+  todo: Todo;
+}
+export default todoSlice.reducer;
+
+
+ 
