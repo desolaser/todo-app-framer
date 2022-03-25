02f178da801d92c962680532f41bc56821922b52
Felipe Olavarria <178.felipe@gmail.com>
2022-03-25 15:36:25 -0300

    Import todo as default in todoSlice

--- redux/slices/todoSlice.ts
+++ redux/slices/todoSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
-import { Todo } from "../../model/Todo";
+import Todo from "../../model/Todo";
 
 const initialState: Todo[] = []
 
