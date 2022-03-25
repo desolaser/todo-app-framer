d4640c2e6e1db05c29bdf19bccb2e2fd7871a992
Felipe Olavarria <178.felipe@gmail.com>
2022-03-25 15:37:00 -0300

    Change props in task component

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,36 +1,35 @@
-import { FunctionComponent } from "react"
+import type { FunctionComponent } from 'react';
 import { 
   Box, 
   Heading, 
   Text, 
   Button, 
   useColorModeValue, 
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import Todo from '../model/Todo';
 
 interface TaskProps {
-  id: string,
-  title: string,
-  description: string,
-  handleComplete: any,
-  handleDelete: any
-}
+  todo: Todo,
+  handleComplete: (taskId: number) => void,
+  handleDelete: (taskId: number) => void
+};
 
-const Task: FunctionComponent<TaskProps> = ({ id, title, description, handleComplete, handleDelete }) => {
-  const boxColorItem = useColorModeValue('gray.300', 'gray.600')
+const Task: FunctionComponent<TaskProps> = ({ todo, handleComplete, handleDelete }) => {
+  const boxColorItem = useColorModeValue('gray.300', 'gray.600');
   return (
     <Box p="1rem" bg={boxColorItem} rounded="xl">
-      <Heading size="lg" mb={1}>{title}</Heading>            
+      <Heading size="lg" mb={1}>{todo.title}</Heading>            
       <Text fontSize='lg'>
-        {description}
+        {todo.description}
       </Text>
-      <Button size='md' colorScheme='green' mt={2} mr={2} onClick={handleComplete}>
+      <Button size='md' colorScheme='green' mt={2} mr={2} onClick={e => handleComplete(todo.id)}>
         Complete Task
       </Button>
-      <Button size='md' colorScheme='red' mt={2} onClick={handleDelete}>
-        Borrar
+      <Button size='md' colorScheme='red' mt={2} onClick={e => handleDelete(todo.id)}>
+        Delete
       </Button>
     </Box>
-  )
+  );
 }
 
-export default Task
+export default Task;
