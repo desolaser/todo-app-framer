340b05b3829c8e184b3e21a93e642a6e6e5b24d2
Felipe Olavarria <178.felipe@gmail.com>
2022-03-25 16:56:12 -0300

    Add todo checked display on Task component

--- components/Task.tsx
+++ components/Task.tsx
@@ -4,8 +4,14 @@ import {
   Heading, 
   Text, 
   Button, 
+  Checkbox,
+  HStack,
+  VStack,
   useColorModeValue, 
 } from '@chakra-ui/react';
+import { 
+  CheckIcon  
+} from '@chakra-ui/icons';
 import Todo from '../model/Todo';
 
 interface TaskProps {
@@ -18,10 +24,15 @@ const Task: FunctionComponent<TaskProps> = ({ todo, handleComplete, handleRemove
   const boxColorItem = useColorModeValue('gray.300', 'gray.600');
   return (
     <Box p="1rem" bg={boxColorItem} rounded="xl">
-      <Heading size="lg" mb={1}>{todo.title}</Heading>            
-      <Text fontSize='lg'>
-        {todo.description}
-      </Text>
+      <HStack spacing={8} justify="space-between">
+        <VStack spacing={0} align="flex-start">
+          <Heading size="lg" mb={1}>{todo.title}</Heading>            
+          <Text fontSize='lg'>
+            {todo.description}
+          </Text>
+        </VStack>
+        {todo.isDone && <CheckIcon />}
+      </HStack>
       <Button size='md' colorScheme='green' mt={2} mr={2} onClick={_ => handleComplete(todo.id)}>
         Complete Task
       </Button>
