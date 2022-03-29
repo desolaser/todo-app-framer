644bccccbeffbd2fb85fae4eb38d78cee7117c20
Felipe Olavarria <178.felipe@gmail.com>
2022-03-29 12:48:34 -0300

    Add DragDropContext to app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,21 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
+import { DragDropContext } from 'react-beautiful-dnd';
 import store from '../redux/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const onDragEnd = () => {
+
+  }
+
   return (
     <ChakraProvider>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </DragDropContext>
     </ChakraProvider>
   );
 }
