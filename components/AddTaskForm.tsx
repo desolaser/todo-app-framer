896c2ba26055f7399ae22b8b9c12ebe994298535
Felipe Olavarria <178.felipe@gmail.com>
2022-03-29 10:01:54 -0300

    Remove description field from addtask form

--- components/AddTaskForm.tsx
+++ components/AddTaskForm.tsx
@@ -1,38 +1,31 @@
 import React from 'react';
-import { 
-  FormControl, 
-  Input,
+import {
+  FormControl,
   FormErrorMessage,
-  Textarea, 
+  Textarea,
   Button,
 } from '@chakra-ui/react';
 import useTodo from '../hooks/useTodo';
 
-const AddTaskForm: React.FC = () => {
-  const { formik } = useTodo();
+interface AddTaskFormProps {
+  columnId: string
+}
+
+const AddTaskForm: React.FC<AddTaskFormProps> = ({ columnId }) => {
+  const { addTodoForm } = useTodo(columnId);
 
   return (
-    <form onSubmit={formik.handleSubmit} style={{ width: '100%' }}>
-      <FormControl mb="1rem" isInvalid={'title' in formik.errors}>
-        <Input 
+    <form onSubmit={addTodoForm.handleSubmit} style={{ width: '100%' }}>
+      <FormControl mb="1rem" isInvalid={'title' in addTodoForm.errors}>
+        <Textarea 
           placeholder="Insert text"
           name="title"
-          value={formik.values.title}
-          onChange={formik.handleChange}
+          value={addTodoForm.values.title}
+          onChange={addTodoForm.handleChange}
         />
-        {'title' in formik.errors && 
-          <FormErrorMessage>{formik.errors.title}</FormErrorMessage>}            
-      </FormControl>
-      <FormControl mb="1rem" isInvalid={'description' in formik.errors}>
-        <Textarea 
-          placeholder="Insert description"
-          name="description"
-          value={formik.values.description}
-          onChange={formik.handleChange}
-        />
-        {'description' in formik.errors && 
-          <FormErrorMessage>{formik.errors.description}</FormErrorMessage>}
-      </FormControl>
+        {'title' in addTodoForm.errors && 
+          <FormErrorMessage>{addTodoForm.errors.title}</FormErrorMessage>}            
+      </FormControl>      
       <Button colorScheme={'blue'} type="submit" w="full" mb="1rem">
         Agregar
       </Button>
