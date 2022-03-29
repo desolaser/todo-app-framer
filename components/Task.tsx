99b421244d789662f8c3550599994cd3ac0012ea
Felipe Olavarria <178.felipe@gmail.com>
2022-03-29 08:56:43 -0300

    Separate editform from task

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,17 +1,11 @@
-import { FunctionComponent, useState } from 'react';
-import { useFormik } from 'formik';
+import React, { useState } from 'react';
 import { 
   Box, 
   Heading, 
-  Text, 
-  Button, 
+  Text,
   IconButton,
   HStack,
   VStack,
-  Input,
-  Textarea,
-  FormControl,
-  FormErrorMessage,
   useColorModeValue, 
 } from '@chakra-ui/react';
 import { 
@@ -21,7 +15,7 @@ import {
   EditIcon
 } from '@chakra-ui/icons';
 import Todo from '../model/Todo';
-import useTodo from '../hooks/useTodo';
+import EditTaskForm from '../components/EditTaskForm';
 
 interface TaskProps {
   todo: Todo,
@@ -29,30 +23,9 @@ interface TaskProps {
   handleRemove: (taskId: string) => void
 };
 
-const Task: FunctionComponent<TaskProps> = ({ todo, handleComplete, handleRemove }) => {
+const Task: React.FC<TaskProps> = ({ todo, handleComplete, handleRemove }) => {
   const boxColorItem = useColorModeValue('gray.300', 'gray.600');
   const [editMode, setEditMode] = useState<boolean>(false);
-  const { handleEdit } = useTodo();
-  const editForm = useFormik({
-    initialValues: {
-      title: todo.title,
-      description: todo.description
-    },
-    validate: values => { 
-      let errors = {};
-
-      if (!values.title)
-        errors = { title: "El titulo es obligatorio" };
-      if (!values.description)
-        errors = { ...errors, description: "La descripción es obligatoria" };
-
-      return errors;
-    },
-    onSubmit: values => {
-      handleEdit(todo.id, values.title, values.description);
-      setEditMode(false);
-    },
-  });
 
   const toogleEditMode = () => {
     setEditMode(!editMode);
@@ -71,32 +44,7 @@ const Task: FunctionComponent<TaskProps> = ({ todo, handleComplete, handleRemove
           {todo.isDone && <CheckIcon />}
         </HStack>
       ) : (
-        <form onSubmit={editForm.handleSubmit} style={{ width: '100%' }}>
-          <Input type="hidden" name="id" value={todo.id} />
-          <FormControl mb="1rem" isInvalid={'title' in editForm.errors}>
-            <Input 
-              placeholder="Insert text"
-              name="title"
-              value={editForm.values.title}
-              onChange={editForm.handleChange}
-            />
-            {'title' in editForm.errors && 
-              <FormErrorMessage>{editForm.errors.title}</FormErrorMessage>}            
-          </FormControl>
-          <FormControl mb="1rem" isInvalid={'description' in editForm.errors}>
-            <Textarea 
-              placeholder="Insert description"
-              name="description"
-              value={editForm.values.description}
-              onChange={editForm.handleChange}
-            />
-            {'description' in editForm.errors && 
-              <FormErrorMessage>{editForm.errors.description}</FormErrorMessage>}
-          </FormControl>
-          <Button colorScheme="blue" type="submit" w="full" mb="1rem">
-            Guardar
-          </Button>
-        </form>
+        <EditTaskForm todo={todo} setEditMode={setEditMode} />
       )}
       <HStack justify="space-between">
         <IconButton
