4f0162f93e92b668f51c1a6d53deaf0c031650c2
Felipe Olavarria <178.felipe@gmail.com>
2022-03-29 10:02:13 -0300

    Put Addtaskform in columns display instead of index

--- components/ColumnDisplay.tsx
+++ components/ColumnDisplay.tsx
@@ -3,11 +3,17 @@ import {
   Box, 
   Heading,
   Stack,
+  HStack,
+  IconButton,
   useColorModeValue, 
 } from '@chakra-ui/react';
+import {
+  PlusSquareIcon
+} from '@chakra-ui/icons';
 import Todo from '../model/Todo';
 import Column from '../model/Column';
 import TodoDisplay from '../components/TodoDisplay';
+import AddTaskForm from '../components/AddTaskForm';
 
 interface ColumnProps {
   column: Column,
@@ -25,6 +31,13 @@ const Task: React.FC<ColumnProps> = ({ column, todos }) => {
       <Stack spacing={3}>
         {todos.map(todo => <TodoDisplay key={todo.id} todo={todo} /> )}
       </Stack>
+      <HStack>
+        <Box>
+          Añada una tarea 
+        </Box>
+        <IconButton aria-label='Add card button' icon={<PlusSquareIcon />} />
+      </HStack>
+      <AddTaskForm columnId={column.id} />
     </Box>
   );
 }
