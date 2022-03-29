08faee0a68fd97341c964ba5bdd639053a064933
Felipe Olavarria <178.felipe@gmail.com>
2022-03-29 09:29:04 -0300

    Change add and remove to include the columns in the operations

--- redux/slices/todoSlice.ts
+++ redux/slices/todoSlice.ts
@@ -3,6 +3,16 @@ import type { RootState } from "../store";
 import Todo from "../../model/Todo";
 import Column from "../../model/Column";
 
+type AddPayload = {
+  todo: Todo,
+  columnId: string
+}
+
+type RemovePayload = {
+  todoId: string,
+  columnId: string
+}
+
 const todoSlice = createSlice({
   name: 'todo',
   initialState: {
@@ -11,10 +21,24 @@ const todoSlice = createSlice({
     columnOrder: [] as string[]
   },
   reducers: {
-    add: (state, action: PayloadAction<Todo>) => 
-      { state.todos = [...state.todos, action.payload] },
-    remove: (state, action: PayloadAction<string>) => 
-      { state.todos = state.todos.filter(todo => todo.id !== action.payload) },
+    add: (state, action: PayloadAction<AddPayload>) => { 
+      const { todo, columnId } = action.payload
+      const column: Column | undefined = state.columns.filter(column => column.id === columnId)[0]
+      if (typeof column === "undefined") {
+        return
+      }
+      state.todos.push(todo)
+      column.todoIds.push(todo.id)
+    },
+    remove: (state, action: PayloadAction<RemovePayload>) => {
+      const { todoId, columnId } = action.payload
+      const column: Column | undefined = state.columns.filter(column => column.id === columnId)[0]
+      if (typeof column === "undefined") {
+        return
+      }
+      state.todos = state.todos.filter(todo => todo.id != todoId) 
+      column.todoIds = column.todoIds.filter(id => id != todoId)
+    },
     done: (state, action: PayloadAction<string>) => 
       { state.todos = state.todos.map(todo => todo.id === action.payload ? { ...todo, isDone: !todo.isDone } : todo) },      
     edit: (state, action: PayloadAction<EditTodoPayload>) => { 
@@ -23,12 +47,11 @@ const todoSlice = createSlice({
         title: action.payload.title, 
         description: action.payload.description 
       } : todo)
-    },
-    setTodos: (state, action: PayloadAction<Todo[]>) => { state.todos = action.payload }
+    }
   },
 });
 
-export const { add, remove, done, edit, setTodos } = todoSlice.actions;
+export const { add, remove, done, edit } = todoSlice.actions;
 export const selectTodos = (state: RootState) => state.todo;
 export type EditTodoPayload = {
   id: string;
