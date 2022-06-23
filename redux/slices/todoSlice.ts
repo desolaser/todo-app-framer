d765f6a26e07f74e2bb00319f4563d763bd80ecd
Felipe Olavarria <178.felipe@gmail.com>
2022-06-23 09:12:46 -0400

    Add todoState type in todoSlice

--- redux/slices/todoSlice.ts
+++ redux/slices/todoSlice.ts
@@ -31,15 +31,21 @@ type SwapColumnPayload = {
   destinationIndex: number
 };
 
+type TodoState = {
+  todos: Todo[],
+  columns: Column[],
+  columnOrder: string[]
+}
+
 const todoSlice = createSlice({
   name: 'todo',
   initialState: {
-    todos: [] as Todo[],
-    columns: [] as Column[],
-    columnOrder: [] as string[]
-  },
+    todos: [],
+    columns: [],
+    columnOrder: []
+  } as TodoState,
   reducers: {
-    add: (state, action: PayloadAction<AddPayload>) => { 
+    add: (state: TodoState, action: PayloadAction<AddPayload>) => { 
       const { todo, columnId } = action.payload
       const column: Column | undefined = state.columns.filter(column => column.id === columnId)[0]
       if (typeof column === "undefined") {
@@ -48,7 +54,7 @@ const todoSlice = createSlice({
       state.todos.push(todo)
       column.todoIds.push(todo.id)
     },
-    remove: (state, action: PayloadAction<RemovePayload>) => {
+    remove: (state: TodoState, action: PayloadAction<RemovePayload>) => {
       const { todoId, columnId } = action.payload
       const column: Column | undefined = state.columns.filter(column => column.id === columnId)[0]
       if (typeof column === "undefined") {
@@ -57,32 +63,32 @@ const todoSlice = createSlice({
       state.todos = state.todos.filter(todo => todo.id != todoId)
       column.todoIds = column.todoIds.filter(id => id != todoId)
     },
-    done: (state, action: PayloadAction<string>) =>
+    done: (state: TodoState, action: PayloadAction<string>) =>
       { state.todos = state.todos.map(todo => todo.id === action.payload ? { ...todo, isDone: !todo.isDone } : todo) },     
-    edit: (state, action: PayloadAction<EditTodoPayload>) => {
+    edit: (state: TodoState, action: PayloadAction<EditTodoPayload>) => {
       state.todos = state.todos.map(todo => todo.id === action.payload.id ? {
         ...todo, 
         title: action.payload.title, 
         description: action.payload.description 
       } : todo)
     },
-    addColumn: ({ columns, columnOrder }, action: PayloadAction<Column>) => {
+    addColumn: ({ columns, columnOrder }: TodoState, action: PayloadAction<Column>) => {
       columns.push(action.payload)
       columnOrder.push(action.payload.id)
     },
-    editColumn: (state, action: PayloadAction<EditColumnPayload>) => ({
+    editColumn: (state: TodoState, action: PayloadAction<EditColumnPayload>) => ({
       ...state,
       columns: state.columns.map(column => column.id == action.payload.columnId ? { 
         ...column, 
         title: action.payload.title 
       } : column)
     }),
-    removeColumn: (state, action: PayloadAction<string>) => ({
+    removeColumn: (state: TodoState, action: PayloadAction<string>) => ({
       ...state,
       columns: state.columns.filter(column => column.id != action.payload),
       columnOrder: state.columnOrder.filter(columnId => columnId != action.payload)
     }),
-    swapTodo: (state, action: PayloadAction<SwapTodoPayload>) => {
+    swapTodo: (state: TodoState, action: PayloadAction<SwapTodoPayload>) => {
       const { todoId, sourceColumnId, destinationColumnId, sourceIndex, destinationIndex } = action.payload;      
       let sourceColumn: Column | undefined = state.columns.filter(column => column.id === sourceColumnId)[0];
       if (typeof sourceColumn === "undefined") {
@@ -96,7 +102,7 @@ const todoSlice = createSlice({
       sourceColumn.todoIds.splice(sourceIndex, 1);
       destinationColumn.todoIds.splice(destinationIndex, 0, todoId);
     },
-    swapColumn: (state, action: PayloadAction<SwapColumnPayload>) => {
+    swapColumn: (state: TodoState, action: PayloadAction<SwapColumnPayload>) => {
       const { sourceIndex, destinationIndex } = action.payload;
       const newColumnOrder = Array.from(state.columnOrder);
       
