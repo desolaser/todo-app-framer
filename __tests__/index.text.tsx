e87dfa68705e2f792a6c5ae59c1edeb3e8c325cb
Felipe Olavarria <178.felipe@gmail.com>
2022-06-28 16:04:21 -0400

    Add tests for column creation

--- __tests__/index.text.tsx
+++ __tests__/index.text.tsx
@@ -1,13 +1,32 @@
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
-import { render, screen } from "../utils/test-utils";
+import { render, screen, fireEvent, within, act } from "../utils/test-utils";
 import { setupStore } from '../redux/store';
 
 describe("Test board page", () => {
-  it("Creates a column", () => {
+  it("Creates a column", async () => {
     const store = setupStore();
-
     render(<Home />, { store });
-    expect(screen.getByText(/Add column/i)).toBeInTheDocument();
+
+    // Activates add mode
+    act(() => {
+      const addModeButton = screen.getByLabelText<HTMLButtonElement>('Add card button');
+      fireEvent.click(addModeButton);
+    });
+
+    //Write the column name and checks if the value is changed
+    const columnNameInput = screen.getByPlaceholderText<HTMLInputElement>('Insert text');
+    act(() => {
+      fireEvent.change(columnNameInput, { target: { value: "Column Test 1" } });
+    });
+    expect(columnNameInput.value).toEqual("Column Test 1");
+      
+    // Adds the column to the board and checks if exists    
+    const addColumnButton = screen.getByLabelText("add-column");
+    await act(async () => {
+      fireEvent.click(addColumnButton);
+    });
+    const addedColumn = screen.queryAllByText("Column Test 1");
+    expect(addedColumn).toHaveLength(1);
   });
 });
