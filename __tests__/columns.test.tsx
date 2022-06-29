cb18f33dd71283b12dd483d56d79ae6f41efaa7d
Felipe Olavarria <178.felipe@gmail.com>
2022-06-29 10:56:08 -0400

    Add test for remove and edit column

--- __tests__/columns.test.tsx
+++ __tests__/columns.test.tsx
@@ -4,7 +4,7 @@ import { onDragEnd } from "../lib/beautifulDnD";
 import { setupStore } from '../redux/store';
 import type { AppStore } from '../redux/store';
 import { addColumn } from "../redux/slices/todoSlice";
-import { render, screen, act } from "../utils/test-utils";
+import { render, screen, act, within, fireEvent } from "../utils/test-utils";
 import Home from "../pages/index";
 import Column from "../model/Column";
 
@@ -134,6 +134,48 @@ describe("Test columns functionalities", () => {
     const renderedColumns2 = screen.queryAllByText(/Column test/i);
     expect(renderedColumns2).toHaveLength(2);
     expect(renderedColumns2[0].innerHTML).toEqual("Column test 1");
-    expect(renderedColumns2[1].innerHTML).toEqual("Column test 2");  
+    expect(renderedColumns2[1].innerHTML).toEqual("Column test 2");
+  });
+
+  it ("It should remove a column 2", async () => {
+    const columnsOptions = screen.getAllByLabelText<HTMLButtonElement>("options");
+    act(() => {
+      fireEvent.click(columnsOptions[1]);
+    });
+
+    const deleteButton = screen.getAllByText("Delete");
+    await act(async () => {
+      fireEvent.click(deleteButton[1]);
+    });
+    
+    const renderedColumns = screen.queryAllByText(/Column test/i);
+    expect(renderedColumns).toHaveLength(1);
+    expect(renderedColumns[0].innerHTML).toEqual("Column test 1");
+  });
+
+  it ("It should edit column 2 title", async () => {
+    const columnsOptions = screen.getAllByLabelText<HTMLButtonElement>("options");
+    act(() => {
+      fireEvent.click(columnsOptions[1]);
+    });
+
+    const editButton = screen.getAllByText("Edit");
+    await act(async () => {
+      fireEvent.click(editButton[1]);
+    });
+
+    const editColumnInput = screen.getByPlaceholderText("Change column title");
+    await act(async () => {
+      fireEvent.change(editColumnInput, { target: { value: "Column test 2 edited" } });
+    });
+
+    const saveButton = screen.getByLabelText("save-column");
+    await act(async () => {
+      fireEvent.click(saveButton);
+    });
+    
+    const renderedColumns = screen.queryAllByText(/Column test/i);
+    expect(renderedColumns).toHaveLength(2);
+    expect(renderedColumns[1].innerHTML).toEqual("Column test 2 edited");
   });
 });
