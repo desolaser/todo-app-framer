3ec4e81e25129e089f497fb9d9a6c489d7fbb572
Felipe Olavarria <178.felipe@gmail.com>
2022-06-30 08:45:40 -0400

    Add sign up tests

---
+++ __tests__/auth/sign-up.test.tsx
@@ -0,0 +1,210 @@
+import Router from 'next/router';
+import SingUp from "../../pages/auth/sign-up";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent, act } from "../../utils/test-utils";
+import { setupStore } from '../../redux/store';
+import type { AppStore } from '../../redux/store';
+
+describe("Sign in page tests", () => {  
+  let store: AppStore | null = null;
+
+  let emailInput: HTMLInputElement;
+  let emailConfirmationInput: HTMLInputElement;
+  let passwordInput: HTMLInputElement;
+  let passwordConfirmationInput: HTMLInputElement;
+
+  const spies: any = {};
+
+  beforeEach(() => {
+    store = setupStore();
+    render(<SingUp />, { store });
+
+    emailInput = screen.getByLabelText<HTMLInputElement>('email');
+    emailConfirmationInput = screen.getByLabelText<HTMLInputElement>('email-confirmation');
+    passwordInput = screen.getByLabelText<HTMLInputElement>('password');
+    passwordConfirmationInput = screen.getByLabelText<HTMLInputElement>('password-confirmation');
+
+    spies.routerChangeStart = jest.fn();
+    Router.events.on('routeChangeStart', spies.routerChangeStart);
+  });
+
+  afterEach(() => {
+    Router.events.off('routeChangeStart', spies.routerChangeStart);
+  });
+
+  it("It should sign in if all fields are filled with valid data", async () => {
+    const email: string = "test@test.cl";
+    const password: string = "Aa693341480++--";
+
+    act(() => {
+      fireEvent.change(emailInput, { target: { value: email } });
+      fireEvent.change(emailConfirmationInput, { target: { value: email } });
+      fireEvent.change(passwordInput, { target: { value: password } });
+      fireEvent.change(passwordConfirmationInput, { target: { value: password } });
+    });
+    expect(emailInput.value).toEqual(email);
+    expect(emailConfirmationInput.value).toEqual(email);
+    expect(passwordInput.value).toEqual(password);
+    expect(passwordConfirmationInput.value).toEqual(password);
+    
+    const registerButton = screen.getByText<HTMLButtonElement>('Register');
+    act(() => {
+      fireEvent.click(registerButton);
+    });
+
+    expect(screen.getByText(/Successful sign in/i)).toBeInTheDocument();
+  });
+
+  it("It should not sign in if all fields are empty", async () => {
+    const email: string = "";
+    const password: string = "";
+
+    act(() => {
+      fireEvent.change(emailInput, { target: { value: email } });
+      fireEvent.change(emailConfirmationInput, { target: { value: email } });
+      fireEvent.change(passwordInput, { target: { value: password } });
+      fireEvent.change(passwordConfirmationInput, { target: { value: password } });
+    });
+    expect(emailInput.value).toEqual(email);
+    expect(emailConfirmationInput.value).toEqual(email);
+    expect(passwordInput.value).toEqual(password);
+    expect(passwordConfirmationInput.value).toEqual(password);
+    
+    const registerButton = screen.getByText<HTMLButtonElement>('Register');
+    act(() => {
+      fireEvent.click(registerButton);
+    });
+
+    expect(screen.getByText(/Failed sign in/i)).toBeInTheDocument();
+    expect(screen.getByText(/Email field is required/i)).toBeInTheDocument();
+    expect(screen.getByText(/Email confirmation field is required/i)).toBeInTheDocument();
+    expect(screen.getByText(/Password field is required/i)).toBeInTheDocument();
+    expect(screen.getByText(/Password confirmation field is required/i)).toBeInTheDocument();
+  });
+
+  it("It should not sign in if email field isn't filled", async () => {
+    const email: string = "";
+    const password: string = "Aa693341480++--";
+
+    act(() => {
+      fireEvent.change(emailInput, { target: { value: email } });
+      fireEvent.change(emailConfirmationInput, { target: { value: email } });
+      fireEvent.change(passwordInput, { target: { value: password } });
+      fireEvent.change(passwordConfirmationInput, { target: { value: password } });
+    });
+    expect(emailInput.value).toEqual(email);
+    expect(emailConfirmationInput.value).toEqual(email);
+    expect(passwordInput.value).toEqual(password);
+    expect(passwordConfirmationInput.value).toEqual(password);
+    
+    const registerButton = screen.getByText<HTMLButtonElement>('Register');
+    act(() => {
+      fireEvent.click(registerButton);
+    });
+
+    expect(screen.getByText(/Failed sign in/i)).toBeInTheDocument();
+    expect(screen.getByText(/Email field is required/i)).toBeInTheDocument();
+    expect(screen.getByText(/Email confirmation field is required/i)).toBeInTheDocument();
+  });
+
+  it("It should not sign in if email field is filled and email confirmation is not", async () => {
+    const password: string = "Aa693341480++--";
+
+    act(() => {
+      fireEvent.change(emailInput, { target: { value: "test@test.cl" } });
+      fireEvent.change(emailConfirmationInput, { target: { value: "" } });
+      fireEvent.change(passwordInput, { target: { value: password } });
+      fireEvent.change(passwordConfirmationInput, { target: { value: password } });
+    });
+    expect(emailInput.value).toEqual("test@test.cl");
+    expect(emailConfirmationInput.value).toEqual("");
+    expect(passwordInput.value).toEqual(password);
+    expect(passwordConfirmationInput.value).toEqual(password);
+    
+    const registerButton = screen.getByText<HTMLButtonElement>('Register');
+    act(() => {
+      fireEvent.click(registerButton);
+    });
+
+    expect(screen.getByText(/Failed sign in/i)).toBeInTheDocument();
+    expect(screen.getByText(/Email confirmation field is required/i)).toBeInTheDocument();
+  });
+
+  it("It should not sign in if email and email confirmation fields are different", async () => {
+    const password: string = "Aa693341480++--";
+
+    act(() => {
+      fireEvent.change(emailInput, { target: { value: "test@test.cl" } });
+      fireEvent.change(emailConfirmationInput, { target: { value: "test" } });
+      fireEvent.change(passwordInput, { target: { value: password } });
+      fireEvent.change(passwordConfirmationInput, { target: { value: password } });
+    });
+    expect(emailInput.value).toEqual("test@test.cl");
+    expect(emailConfirmationInput.value).toEqual("test");
+    expect(passwordInput.value).toEqual(password);
+    expect(passwordConfirmationInput.value).toEqual(password);
+    
+    const registerButton = screen.getByText<HTMLButtonElement>('Register');
+    act(() => {
+      fireEvent.click(registerButton);
+    });
+
+    expect(screen.getByText(/Failed sign in/i)).toBeInTheDocument();
+    expect(screen.getByText(/Email confirmation and Email fields are different/i)).toBeInTheDocument();
+  });
+
+  it("It should not sign in if password field is filled and password confirmation is not", async () => {
+    const email: string = "test@test.cl";
+
+    act(() => {
+      fireEvent.change(emailInput, { target: { value: email } });
+      fireEvent.change(emailConfirmationInput, { target: { value: email } });
+      fireEvent.change(passwordInput, { target: { value: "Aa693341480++--" } });
+      fireEvent.change(passwordConfirmationInput, { target: { value: "" } });
+    });
+    expect(emailInput.value).toEqual(email);
+    expect(emailConfirmationInput.value).toEqual(email);
+    expect(passwordInput.value).toEqual("Aa693341480++--");
+    expect(passwordConfirmationInput.value).toEqual("");
+    
+    const registerButton = screen.getByText<HTMLButtonElement>('Register');
+    act(() => {
+      fireEvent.click(registerButton);
+    });
+
+    expect(screen.getByText(/Failed sign in/i)).toBeInTheDocument();
+    expect(screen.getByText(/Password confirmation is required/i)).toBeInTheDocument();
+  });
+
+  it("It should not sign in if password and password confirmation fields are different", async () => {
+    const email: string = "test@test.cl";
+
+    act(() => {
+      fireEvent.change(emailInput, { target: { value: email } });
+      fireEvent.change(emailConfirmationInput, { target: { value: email } });
+      fireEvent.change(passwordInput, { target: { value: "Aa693341480++--" } });
+      fireEvent.change(passwordConfirmationInput, { target: { value: "" } });
+    });
+    expect(emailInput.value).toEqual(email);
+    expect(emailConfirmationInput.value).toEqual(email);
+    expect(passwordInput.value).toEqual("Aa693341480++--");
+    expect(passwordConfirmationInput.value).toEqual("");
+    
+    const registerButton = screen.getByText<HTMLButtonElement>('Register');
+    act(() => {
+      fireEvent.click(registerButton);
+    });
+
+    expect(screen.getByText(/Failed sign in/i)).toBeInTheDocument();
+    expect(screen.getByText(/Password confirmation and password fields are different/i)).toBeInTheDocument();
+  });
+
+  it("It should redirect to sign in page if sign in link is pressed", async () => {
+    const signInLink = screen.getByText(/You have an account already? Click here/i);
+    act(() => {
+      fireEvent.click(signInLink);
+    });
+    
+    expect(spies.routerChangeStart).toHaveBeenCalledWith('/auth/sign-in');
+  });
+});
